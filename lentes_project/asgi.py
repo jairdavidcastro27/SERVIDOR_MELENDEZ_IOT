46d37f1e2789e1f26327b9c086f5e1c611ed78c4
jair david <cjair759@gmail.com>
2025-11-11 00:34:57 -0500

    se hace la web y wl front 5

--- lentes_project/asgi.py
+++ lentes_project/asgi.py
@@ -1,13 +1,16 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
+from channels.auth import AllowedHostsOriginValidator
 import core.routing
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lentes_project.settings')
 
+# La aplicación ASGI principal
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    # AllowedHostsOriginValidator envuelve nuestro router para proteger contra ataques CSRF
+    # a través de WebSockets, verificando el origen contra ALLOWED_HOSTS.
+    "websocket": AllowedHostsOriginValidator(
         URLRouter(
             core.routing.websocket_urlpatterns
         )
